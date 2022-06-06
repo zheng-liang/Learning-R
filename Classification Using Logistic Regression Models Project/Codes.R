ca91646b92f5d78c52d19de21edaa7ed229b41be
zhengliang <zltan2131@gmail.com>
2022-06-07 00:34:30 +0800

    update with new code and sections

--- Classification Using Logistic Regression Models Project/Codes.R
+++ Classification Using Logistic Regression Models Project/Codes.R
@@ -1,12 +1,14 @@
 
-# Load packages
+# Load packages ----
 library(car) 
 library(caret) # For building ML models
 library(corrplot) # For correlation plot
 library(tidyverse) # For ggplot and dplyr packages
 library(mlbench) # For data sets
+library(lmtest) # For statistical tests
+library(glmnet) # For LASSO logistic regression
 
-# Exploratory Data Analysis
+# Exploratory Data Analysis ----
 ## Load data and summarize
 data("PimaIndiansDiabetes")
 str(PimaIndiansDiabetes)
@@ -69,37 +71,37 @@ corrplot(corr = corrX,
          title = "Correlation Between Explanatory Variables",
          mar = c(0, 0, 1, 0))
 
-# Building the logistic regression model
+# Building the logistic regression model ----
 ## Full model
-model1 <- glm(diabetes ~ ., 
+model1 <- glm(diabetes == "pos" ~ ., 
               data = data, 
               family = binomial(link = "logit"))
 
 summary(model1)
 
 ## Restricted Model
-model2 <- glm(diabetes ~ pregnant + glucose + mass + pedigree,
+model2 <- glm(diabetes == "pos" ~ pregnant + glucose + mass + pedigree,
               data = data,
               family = binomial(link = "logit"))
 
 summary(model2)
 
-# Likelihood Ratio test
+## Likelihood Ratio test
 lmtest::lrtest(model2, model1)
 
 # VIF to check for multicollinearity
 car::vif(model2)
 
 # Predict probabilities using model2
-prob <- fitted.values(model2)
+prob.model2 <- fitted.values(model2)
 
-# Calculate logit and bind into a new data frame with variables for plotting
+## Calculate logit and bind into a new data frame with variables for plotting
 pred <- names(coef(model2)[-1])
 
 plotdata <- subset(data, select = pred)
 
 plotdata <- plotdata %>%
-  mutate(logit = log(prob/(1-prob))) %>%
+  mutate(logit = log(prob.model2/(1-prob.model2))) %>%
   gather(key = "pred", value = "pred.value", -logit)
 
 ggplot(data = plotdata, aes(x = logit, y = pred.value)) +
@@ -107,36 +109,133 @@ ggplot(data = plotdata, aes(x = logit, y = pred.value)) +
   geom_smooth(method = "loess") +
   facet_wrap(. ~ pred, scales = "free")
 
-# Transforming pedigree to make it linear to the log-odds
-model3 <- glm(diabetes ~ pregnant + glucose + mass + log(pedigree),
+## Transforming pedigree to make it linear to the log-odds
+model3 <- glm(diabetes == "pos" ~ pregnant + glucose + mass + log(pedigree),
               data = data,
               family = binomial(link = "logit"))
 
 summary(model3)
 
-prob2 <- fitted.values(model3)
-logit = log(prob2/(1-prob2))
+prob.model3 <- fitted.values(model3)
+logit = log(prob.model3/(1-prob.model3))
 
 plotdata2 <- subset(data, select = pred)
 
 plotdata2 <- plotdata2 %>%
-  mutate(l.pedigree = log(pedigree), pedigree = NULL)
+  mutate(l.pedigree = log(pedigree))
 
 ggplot(data = plotdata2, aes(x = logit, y = l.pedigree)) +
   geom_point(alpha = 0.7) +
   geom_smooth(method = "loess")
 
-# Find influential values in model 3 by visualizing Cook's Distance
+# Checking for Influential Outliers ----
+
+## Find outliers using standardized residual errors
+std.resid <- rstandard(model3)
+val <- abs(std.resid) > 3
+table(val)["TRUE"]
+
+outlierTest(model3)
+
+## Find influential values in model 3 by visualizing Cook's Distance
 plot(model3, which = 4)
+sort(cooks.distance(model3), decreasing = T)
 
-# Find outliers using standardized residual errors
-model3data <- augment(model3) %>%
-  mutate(index = 1:n())
+# Evaluating the Models -----
+
+## Storing fitted values
+prob.model1 <- fitted.values(model1) 
+prediction.model1 <- as.factor(ifelse(prob.model1 > 0.5, "pos", "neg"))
+confusionMatrix(data$diabetes, prediction.model1)
+
+prediction.model2 <- as.factor(ifelse(prob.model2 > 0.5, "pos", "neg"))
+confusionMatrix(data$diabetes, prediction.model2)
+
+prediction.model3 <- as.factor(ifelse(prob.model3 > 0.5, "pos", "neg"))
+confusionMatrix(data$diabetes, prediction.model3)
+
+# Using other sources of data ----
+
+## Load package
+library(readxl)
+
+## Load data from wd
+default.data <- read_excel("credit card default.xls")
+
+## Head and tail of data
+head(default.data)
+
+tail(default.data)
+
+colnames(default.data)
+
+## Adjust column names
+colnames(default.data) <- default.data[1,]
+default.data <- default.data[-1,]
+head(default.data)
+
+## Adjust row names
+default.data <- default.data[,-1]
+head(default.data)
+
+temp.df <- default.data
+
+### Renaming columns
+newnames <- c("PAY_SEP","PAY_AUG","PAY_JUL", "PAY_JUN", "PAY_MAY", "PAY_APR",
+              "BILLAMT_SEP","BILLAMT_AUG","BILLAMT_JUL", "BILLAMT_JUN", "BILLAMT_MAY", "BILLAMT_APR",
+              "PAYAMT_SEP","PAYAMT_AUG","PAYAMT_JUL", "PAYAMT_JUN", "PAYAMT_MAY", "PAYAMT_APR")
+
+oldnames <-colnames(default.data %>%
+                      dplyr::select(grep("PAY" , names(default.data)), 
+                                    grep("BILL", names(default.data)))) 
+
+colnames(default.data)[colnames(default.data) %in% oldnames] <- newnames
+
+default.data <- default.data %>%
+  rename(default = `default payment next month`)
+
+colnames(default.data)
+
+# EDA of Credit Card Default Data ----
+
+str(default.data)
+
+## Changing to appropriate value type
+temp.df <- default.data # Save to backup
+
+# To make it simple we can change all to numeric first, then to factors
+default.data <- default.data %>% 
+  mutate(across(where(is.character), as.numeric))
+
+str(default.data)
+
+default.data <- default.data %>%
+  mutate(
+    across(
+      c("SEX", "EDUCATION", "MARRIAGE", "default", 
+             grep("PAY_", names(default.data))), 
+      as.factor))
+
+str(default.data)
+
+temp.df <- default.data # Save progress of dataset
+
+## View summary of data
+summary(default.data)
+
+### Replace unknown class with Others
 
 
 
 
 
+## Univariate Plots
+ggplot(data = default.data) +
+  geom_bar(aes(x = SEX, fill = default))
+
+# 
+ggplot(data = default.data) +
+  geom_bar(aes(x = EDUCATION, fill = default))
 
 
 
