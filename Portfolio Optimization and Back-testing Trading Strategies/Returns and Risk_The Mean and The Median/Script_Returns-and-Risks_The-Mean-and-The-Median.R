96f25e6ccb85d18a9dfd3dd6070f85912c69dde4
zhengliang <zltan2131@gmail.com>
2022-11-29 18:35:11 +0800

    update to project

--- Portfolio Optimization and Back-testing Trading Strategies/Returns and Risk_The Mean and The Median/Script_Returns-and-Risks_The-Mean-and-The-Median.R
+++ Portfolio Optimization and Back-testing Trading Strategies/Returns and Risk_The Mean and The Median/Script_Returns-and-Risks_The-Mean-and-The-Median.R
@@ -22,23 +22,23 @@ price_data <- NULL
 for(t in tickers) {
   price_data <- cbind(price_data,
                       quantmod::getSymbols(Symbols = t, src = "yahoo", auto.assign = FALSE,
-                                           from = startdate, to = enddate, periodicity = "weekly") %>% Ad())
+                                           from = startdate, to = enddate, periodicity = "daily") %>% Ad())
 }
 
 # Check dimension of object, start and end date of data collected
 dim(price_data); start(price_data); end(price_data)
 
 # See first 6 observations in price_data
-head(price_data)
+data.frame(head(price_data))
 
 ## ----calculate discrete returns-----------------------------------------------
 return_data <- na.omit(PerformanceAnalytics::Return.calculate(prices = price_data, method = "discrete")) %>%
-  `colnames<-`(paste(tickers, "Return", sep = "."))
+  `colnames<-`(paste("R", tickers, sep = "_"))
 
-dim(return_data); head(return_data)
+dim(return_data); data.frame(head(return_data))
 
 ## ----plot historical returns of WMT-------------------------------------------
-chart.Histogram(R = return_data$PG.Return, 
+chart.Histogram(R = return_data$R_PG, 
                 method = c("add.density", "add.normal", "add.qqplot"), 
                 main = "Density Plot of PG Historical Returns")
 
@@ -86,17 +86,17 @@ portspec <- PortfolioAnalytics::add.constraint(portfolio = portspec,
                                                type = "weight_sum",
                                                min_sum = 1, max_sum = 1)
 
-# Weight of each portfolio component can vary between minimum of 0% and maximum of 25%
+# Weight of each portfolio component can vary between minimum of 0% and maximum of 100%
 portspec <- PortfolioAnalytics::add.constraint(portfolio = portspec,
                                                type="box", 
-                                               min = 0, max = 0.25)
+                                               min = 0, max = 1)
 
 portspec
 
 set.seed(43594)
 
 rand_port <- PortfolioAnalytics::random_portfolios(portfolio = portspec, 
-                                                   permutations = 20000, 
+                                                   permutations = 50000, 
                                                    rp_method = "sample", 
                                                    eliminate = TRUE)
 
@@ -146,7 +146,7 @@ rownames(maxmean_weight) <- paste("OP", 1:nrow(maxmean_weight), sep = "")
 
 data.frame(maxmean_weight)
 
-## ----weekly return of best mean portfolio-------------------------------------
+## ----daily return of best mean portfolio--------------------------------------
 # Returns of best mean portfolio in ret_period using weights from opt_period
 maxmean_returns <- foreach(i = ret_periods, j = 1:nrow(maxmean_weight), .combine = "rbind") %do% {
   tmp <- PerformanceAnalytics::Return.portfolio(R = return_data[i, ], 
@@ -167,7 +167,7 @@ rownames(maxmed_weight) <- paste("OP", 1:nrow(maxmed_weight), sep = "")
 
 data.frame(maxmed_weight)
 
-## ----weekly return of best median portfolio-----------------------------------
+## ----daily return of best median portfolio------------------------------------
 # Returns of best median portfolio in ret_period using weights from opt_period
 maxmed_returns <- foreach(i = ret_periods, j = 1:nrow(maxmed_weight), .combine = "rbind") %do% {
   tmp <- PerformanceAnalytics::Return.portfolio(R = return_data[i, ], 
@@ -196,11 +196,9 @@ chart.Drawdown(R = best_return, geometric = TRUE,
                main = "Drawdown of Best Return Portfolios")
 
 ## ----perf summary of best return portfolios-----------------------------------
-table.AnnualizedReturns(R = best_return, scale = 52, Rf = 0.025/52, geometric = TRUE)
+table.AnnualizedReturns(R = best_return, scale = 252, Rf = 0.03/252, geometric = TRUE)
 
-table.DownsideRisk(R = best_return, scale = 52, Rf = 0.025/52, MAR = 0.07/52)
-
-table.Stats(R = best_return)
+table.DownsideRisk(R = best_return, scale = 252, Rf = 0.03/252, MAR = 0.08/252)
 
 ## ----find weights for minimum variance----------------------------------------
 minstd_weight <- foreach(i = opt_periods, .combine = "rbind") %do% {
@@ -213,7 +211,7 @@ rownames(minstd_weight) <- paste("OP", 1:nrow(minstd_weight), sep = "")
 
 data.frame(minstd_weight)
 
-## ----weekly return of minimum variance portfolio------------------------------
+## ----daily return of minimum variance portfolio-------------------------------
 minstd_returns <- foreach(i = ret_periods, j = 1:nrow(minstd_weight), .combine = "rbind") %do% {
   tmp <- PerformanceAnalytics::Return.portfolio(R = return_data[i, ], 
                                                 weights = minstd_weight[j, ], 
@@ -234,7 +232,7 @@ rownames(minmad_weight) <- paste("OP", 1:nrow(minmad_weight), sep = "")
 
 data.frame(minmad_weight)
 
-## ----weekly return of minimum mad portfolio-----------------------------------
+## ----daily return of minimum mad portfolio------------------------------------
 minmad_returns <- foreach(i = ret_periods, j = 1:nrow(minmad_weight), .combine = "rbind") %do% {
   tmp <- PerformanceAnalytics::Return.portfolio(R = return_data[i, ], 
                                                 weights = minmad_weight[j, ], 
@@ -255,7 +253,7 @@ rownames(minmead_weight) <- paste("OP", 1:nrow(minmead_weight), sep = "")
 
 data.frame(minmead_weight)
 
-## ----weekly return of minimum mead portfolio----------------------------------
+## ----daily return of minimum mead portfolio-----------------------------------
 minmead_returns <- foreach(i = ret_periods, j = 1:nrow(minmead_weight), .combine = "rbind") %do% {
   tmp <- PerformanceAnalytics::Return.portfolio(R = return_data[i, ], 
                                                 weights = minmead_weight[j, ], 
@@ -286,197 +284,7 @@ chart.Drawdown(R = min_risk, geometric = TRUE,
                main = "Drawdown of Minimum Risk Portfolios")
 
 ## ----perf summary of minimum risk portfolios----------------------------------
-table.AnnualizedReturns(R = min_risk, scale = 52, Rf = 0.025/52, geometric = TRUE)
+table.AnnualizedReturns(R = min_risk, scale = 252, Rf = 0.03/252, geometric = TRUE)
 
-table.DownsideRisk(R = min_risk, scale = 52, Rf = 0.025/52, MAR = 0.07/52)
-
-table.Stats(R = min_risk)
-
-## ----find rand ports with mean return above ewp mean return-------------------
-mean_port <- foreach(i = opt_periods) %do% {
-  tmp <- PerformanceAnalytics::Mean.arithmetic(x = rp_returns[i, ])
-  
-  ewp_mean <- mean(x = ewp_return[i, ])
-  
-  mean_target <- rand_port[which(tmp > ewp_mean),]
-}
-
-## ----find rand ports with median return above ewp median return---------------
-median_port <- foreach(i = opt_periods) %do% {
-  tmp <- apply(X = rp_returns[i, ], MARGIN = 2, FUN = median)
-  
-  ewp_median <- median(x = ewp_return[i, ])
-  
-  median_target <- rand_port[which(tmp > ewp_median),]
-}
-
-## ----weekly return of subset portfolios---------------------------------------
-# Find returns from rp_returns that satisfy the target mean return constraint
-meanport_return <- foreach(i = opt_periods) %do% {
-  tmp <- PerformanceAnalytics::Mean.arithmetic(x = rp_returns[i, ])
-  
-  ewp_mean <- PerformanceAnalytics::Mean.arithmetic(x = ewp_return[i, ])
-  
-  mean_target <- rp_returns[i, which(tmp > as.vector(ewp_mean))]
-}
-
-# Find returns from rp_returns that satisfy the target median return constraint
-medianport_return <- foreach(i = opt_periods) %do% {
-  tmp <- apply(X = rp_returns[i, ], MARGIN = 2, FUN = median)
-  
-  ewp_median <- median(x = ewp_return[i, ])
-  
-  median_target <- rp_returns[i, which(tmp > ewp_median)]
-}
-
-## ----find weights for mean variance portfolio---------------------------------
-meanvar_weight <- foreach(i = 1:length(meanport_return), .combine = "rbind") %do% {
-  tmp <- PerformanceAnalytics::StdDev(R = meanport_return[[i]])
-  
-  opt_weight <- mean_port[[i]][which.min(tmp), ]
-}
-
-rownames(meanvar_weight) <- paste("OP", 1:nrow(meanvar_weight), sep = "")
-
-data.frame(meanvar_weight)
-
-## ----weekly return of mean variance portfolio---------------------------------
-meanvar_returns <- foreach(i = ret_periods, j = 1:nrow(meanvar_weight), .combine = "rbind") %do% {
-  tmp <- PerformanceAnalytics::Return.portfolio(R = return_data[i, ], 
-                                                weights = meanvar_weight[j, ], 
-                                                geometric = TRUE, 
-                                                rebalance_on = "quarters")
-}
-
-## ----find weights for mean mad portfolio--------------------------------------
-meanmad_weight <- foreach(i = 1:length(meanport_return), .combine = "rbind") %do% {
-  tmp <- apply(X = meanport_return[[i]], MARGIN = 2, FUN = function(x) {
-    mean(abs(x - mean(x)))
-    })
-  
-  opt_weight <- mean_port[[i]][which.min(tmp), ]
-}
-
-rownames(meanmad_weight) <- paste("OP", 1:nrow(meanmad_weight), sep = "")
-
-data.frame(meanmad_weight)
-
-## ----weekly return of mean mad portfolio--------------------------------------
-meanmad_returns <- foreach(i = ret_periods, j = 1:nrow(meanmad_weight), .combine = "rbind") %do% {
-  tmp <- PerformanceAnalytics::Return.portfolio(R = return_data[i, ], 
-                                                weights = meanmad_weight[j, ], 
-                                                geometric = TRUE, 
-                                                rebalance_on = "quarters")
-}
-
-## ----find weights for mean mead portfolio-------------------------------------
-meanmead_weight <- foreach(i = 1:length(meanport_return), .combine = "rbind") %do% {
-  tmp <- apply(X = meanport_return[[i]], MARGIN = 2, FUN = function(x) {
-    median(abs(x - median(x)))
-    })
-  
-  opt_weight <- mean_port[[i]][which.min(tmp), ]
-}
-
-rownames(meanmead_weight) <- paste("OP", 1:nrow(meanmead_weight), sep = "")
-
-data.frame(meanmead_weight)
-
-## ----weekly return of mean mead portfolio-------------------------------------
-meanmead_returns <- foreach(i = ret_periods, j = 1:nrow(meanmead_weight), .combine = "rbind") %do% {
-  tmp <- PerformanceAnalytics::Return.portfolio(R = return_data[i, ], 
-                                                weights = meanmead_weight[j, ], 
-                                                geometric = TRUE, 
-                                                rebalance_on = "quarters")
-}
-
-## ----find weights for median variance portfolio-------------------------------
-medvar_weight <- foreach(i = 1:length(medianport_return), .combine = "rbind") %do% {
-  tmp <- PerformanceAnalytics::StdDev(R = medianport_return[[i]])
-  
-  opt_weight <- median_port[[i]][which.min(tmp), ]
-}
-
-rownames(medvar_weight) <- paste("OP", 1:nrow(medvar_weight), sep = "")
-
-data.frame(medvar_weight)
-
-## ----weekly return of median variance portfolio-------------------------------
-medvar_returns <- foreach(i = ret_periods, j = 1:nrow(medvar_weight), .combine = "rbind") %do% {
-  tmp <- PerformanceAnalytics::Return.portfolio(R = return_data[i, ], 
-                                                weights = medvar_weight[j, ], 
-                                                geometric = TRUE, 
-                                                rebalance_on = "quarters")
-}
-
-## ----find weights for median mad portfolio------------------------------------
-medmad_weight <- foreach(i = 1:length(medianport_return), .combine = "rbind") %do% {
-  tmp <- apply(X = medianport_return[[i]], MARGIN = 2, FUN = function(x) {
-    mean(abs(x - mean(x)))
-    })
-  
-  opt_weight <- median_port[[i]][which.min(tmp), ]
-}
-
-rownames(medmad_weight) <- paste("OP", 1:nrow(medmad_weight), sep = "")
-
-data.frame(medmad_weight)
-
-## ----weekly return of median mad portfolio------------------------------------
-medmad_returns <- foreach(i = ret_periods, j = 1:nrow(medmad_weight), .combine = "rbind") %do% {
-  tmp <- PerformanceAnalytics::Return.portfolio(R = return_data[i, ], 
-                                                weights = medmad_weight[j, ], 
-                                                geometric = TRUE, 
-                                                rebalance_on = "quarters")
-}
-
-## ----find weights for median mead portfolio-----------------------------------
-medmead_weight <- foreach(i = 1:length(medianport_return), .combine = "rbind") %do% {
-  tmp <- apply(X = medianport_return[[i]], MARGIN = 2, FUN = function(x) {
-    median(abs(x - median(x)))
-    })
-  
-  opt_weight <- median_port[[i]][which.min(tmp), ]
-}
-
-rownames(medmead_weight) <- paste("OP", 1:nrow(medmead_weight), sep = "")
-
-data.frame(medmead_weight)
-
-## ----weekly return of median mead portfolio-----------------------------------
-medmead_returns <- foreach(i = ret_periods, j = 1:nrow(medmead_weight), .combine = "rbind") %do% {
-  tmp <- PerformanceAnalytics::Return.portfolio(R = return_data[i, ], 
-                                                weights = medmead_weight[j, ], 
-                                                geometric = TRUE, 
-                                                rebalance_on = "quarters")
-}
-
-## ----plot weights of optimal portfolios---------------------------------------
-optport_weights <- list(Mean_Variance = meanvar_weight, Mean_MAD = meanmad_weight, Mean_MeAD = meanmead_weight,
-                        Median_Variance = medvar_weight, Median_MAD = medmad_weight, Median_MeAD = medmead_weight)
-
-for(i in 1:6) {
-  chart.StackedBar(w = optport_weights[[i]], colorset = RColorBrewer::brewer.pal(n = 10, "Spectral"),
-                 main = paste("Weights of", gsub(pattern = "_", replacement = " ", names(optport_weights)[i]), "Portfolio", sep = " "), 
-                 ylab = "Weight")
-}
-
-## ----plot returns of optimal portfolios---------------------------------------
-opt_port <- cbind(meanvar_returns, meanmad_returns, meanmead_returns, medvar_returns, medmad_returns, medmead_returns, ewp_return["2016/"]) %>%
-  `colnames<-`(c(names(optport_weights), "Equal Weight"))
-
-chart.CumReturns(R = opt_port, geometric = TRUE, 
-                 main = "Cumulative Return of Optimal Portfolios", 
-                 plot.engine = "plotly")
-
-chart.Drawdown(R = opt_port, geometric = TRUE, 
-               main = "Drawdown of Optimal Portfolios", 
-               plot.engine = "plotly")
-
-## ----perf summary of optimal portfolios---------------------------------------
-table.AnnualizedReturns(R = opt_port, scale = 52, Rf = 0.025/52, geometric = TRUE)
-
-table.DownsideRisk(R = opt_port, scale = 52, Rf = 0.025/52, MAR = 0.07/52)
-
-table.Stats(R = opt_port)
+table.DownsideRisk(R = min_risk, scale = 252, Rf = 0.03/252, MAR = 0.08/252)
 
